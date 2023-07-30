5c2c1845410a5e54fa2fca590bec0e9d7d4c293d
Jerico James Viteño <93725569+jericovtn@users.noreply.github.com>
2023-07-31 01:49:29 +0800

    Add files via upload

---
+++ Student_Information.js
@@ -0,0 +1,71 @@
+// STUDENT 1
+const student1Info = {
+    name: "Jerico James F. Viteño",
+    studentNumber: "2021-05862-MN-0",
+    program: "Bachelor of Science in Computer Engineering",
+    school: "Polytechnic University of the Philippines - Manila",
+    birthdate: " July 30, 2002",
+    contactNumber: "(+63) 905 454 6339",
+    address: "Antipolo City, Philippines",
+
+    information(){
+        console.log("STUDENT 1 INFORMATION")
+        console.log("Name:\t\t\t", this.name)
+        console.log("Student Number: \t", this.studentNumber)
+        console.log("Program: \t\t", this.program)
+        console.log("School: \t\t", this.school)
+        console.log("Birthdate: \t\t", this.birthdate)
+        console.log("Contact Number:\t\t", this.contactNumber)
+        console.log("Current Address:\t", this.address)
+    }
+};
+
+student1Info.information();
+
+// STUDENT 2
+const student2Info = {
+    name: "Erika Jane T. Reyes",
+    studentNumber: "2021-05852-MN-0",
+    program: "Bachelor of Science in Computer Engineering",
+    school: "Polytechnic University of the Philippines - Manila",
+    birthdate: " July 4, 2003",
+    contactNumber: "(+63) 965 759 0070",
+    address: "Parañaque City, Philippines",
+
+    information(){
+        console.log("\nSTUDENT 2 INFORMATION")
+        console.log("Name:\t\t\t", this.name)
+        console.log("Student Number: \t", this.studentNumber)
+        console.log("Program: \t\t", this.program)
+        console.log("School: \t\t", this.school)
+        console.log("Birthdate: \t\t", this.birthdate)
+        console.log("Contact Number:\t\t", this.contactNumber)
+        console.log("Current Address:\t", this.address)
+    }
+};
+
+student2Info.information();
+
+// STUDENT 3
+const student3Info = {
+    name: "Beverly Ann L. Rodriguex",
+    studentNumber: "2021-05853-MN-0",
+    program: "Bachelor of Science in Computer Engineering",
+    school: "Polytechnic University of the Philippines - Manila",
+    birthdate: " June 30, 2003",
+    contactNumber: "(+63) 909 693 7644",
+    address: "Laguna, Philippines",
+
+    information(){
+        console.log("\nSTUDENT 3 INFORMATION")
+        console.log("Name:\t\t\t", this.name)
+        console.log("Student Number: \t", this.studentNumber)
+        console.log("Program: \t\t", this.program)
+        console.log("School: \t\t", this.school)
+        console.log("Birthdate: \t\t", this.birthdate)
+        console.log("Contact Number:\t\t", this.contactNumber)
+        console.log("Current Address:\t", this.address)
+    }
+};
+
+student3Info.information();
